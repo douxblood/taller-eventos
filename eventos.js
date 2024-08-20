0fa5eee658c9cec66cf6f595cc1ac5e0f1fe8a99
douxblood <jisnetsant@gmail.com>
2024-08-20 16:50:41 -0300

    Eventos agregados

---
+++ eventos.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = 
+    document.getElementById('greetingButton');
+
+    button.addEventListener('click', () => {
+        alert('Hola!');
+    });
+
+    const buttonContainer = 
+    document.querySelector('.button-container');
+
+    buttonContainer.addEventListener('click', () => {
+        alert('Hola!, soy el div');
+    });
+});
